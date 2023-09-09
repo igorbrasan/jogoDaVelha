214372692f60fd91067026f0e91b08457a947210
igor <igorbraga.rsantos@gmail.com>
2023-09-09 16:41:07 -0300

    test

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from array import array
+
+import switch
+
+
 def verificarLinhas():
     for i in range (3):
         if tabuleiro[i][0] == tabuleiro[i][1] and tabuleiro[i][1] == tabuleiro[i][2]:
+
             print("fim de jogo!")
-
+def verificador():
+    switch
 print ('hello');
-
-tabuleiro = [['e','f','c'],
+tabuleiro = [["",'f','c'],
              ['d','s','a'],
              ['b','b','b']]
 print(tabuleiro[0])
 print(tabuleiro[1])
 print(tabuleiro[2])
 verificarLinhas()
+#teste comentário?
